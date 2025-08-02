3315079a7368a900d80acfaff32fdf33ed5f326a
Thomas McGrew <tjmcgrew@gmail.com>
2025-08-02 03:32:36 -0400

    Support for SSA4 subtitle files
    Support for SSA4 subtitles
    Better support for SRT
    Improvements to OCR for subpicture conversion

---
+++ ffmpeg.py
@@ -0,0 +1,144 @@
+
+import json
+import subprocess
+from shutil import which
+
+
+def info(filename: str):
+    ffprobe = subprocess.Popen(['ffprobe', '-show_chapters', '-show_streams',
+                                '-print_format', 'json', filename],
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.DEVNULL)
+    ffprobe.wait()
+    return json.loads(ffprobe.stdout.read().decode('utf-8'))
+
+
+class Ffmpeg:
+    def __init__(self, output_file: str, command: str | None = None):
+        if command:
+            self.command = command
+        else:
+            self.command = which('ffmpeg')
+        self.output_file = output_file
+        self.inputs = []
+        self.output_map = []
+        self.filters = []
+        self.loglevel = None
+        self.skip_streams = []
+        self.extra_arguments = []
+        self.start_time = 0
+        self.end_time = 0
+
+    def time_range(self, start: float | int = 0, end: float | int = 0):
+        self.start_time = start
+        self.end_time = end
+
+    def loglevel(self, loglevel: str):
+        self.set_loglevel = loglevel
+        return self
+
+    def input(self, filename: str, stream: int | str | None = None):
+        self.inputs.append(_Ffmpeg_input(filename))
+        if stream is not None:
+            self.map(len(self.inputs), stream)
+        return self.inputs[-1]
+
+    def map(self, index, stream=None):
+        self.output_map.append(_Ffmpeg_output_map(index, stream))
+        return self.output_map[-1]
+
+    def filter_complex(self, filter_str: str):
+        self.filters.append(_Ffmpeg_filter_complex(filter_str))
+
+    def skip(self, stream_type):
+        if stream_type in ('video', 'audio', 'subtitle', 'data'):
+            self.skip_streams.append(stream_type)
+        return self
+
+    def extra_args(self, *args):
+        self.extra_arguments.extend(args)
+        return self
+
+    def get_command(self):
+        command = [self.command, '-y']
+        if self.loglevel:
+            command.append('-loglevel')
+            command.append(self.loglevel)
+        for i in self.inputs:
+            command.extend(i.args())
+        for fc in self.filters:
+            command.extend(fc.args())
+        for o in self.output_map:
+            command.extend(o.args())
+        for s in self.skip_streams:
+            if s == 'video':
+                command.append('-vn')
+            if s == 'audio':
+                command.append('-an')
+            if s == 'subtitle':
+                command.append('-sn')
+            if s == 'data':
+                command.append('-dn')
+        command.extend(self.extra_arguments)
+        if (self.start_time > 0):
+            command.append('-ss')
+            command.append(str(self.start_time))
+        if (self.end_time > 0):
+            command.append('-to')
+            command.append(str(self.end_time))
+        command.append(self.output_file)
+        return command
+
+    def run(self):
+        print(' '.join(self.get_command()))
+        proc = subprocess.Popen(
+                self.get_command(),  # stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL)
+        proc.wait()
+
+
+class _Ffmpeg_input:
+    def __init__(self, filename: str, file_format: str | None = None):
+        self.filename = filename
+        self.file_format = file_format
+        self.time_offset = 0
+
+    def format(self, file_format):
+        self.file_format = file_format
+        return self
+
+    def time_offset(self, offset: float):
+        self.offset = offset
+        return self
+
+    def args(self):
+        args = []
+        if self.file_format:
+            args.append('-f')
+            args.append(self.file_format)
+        if self.time_offset:
+            args.append('-itsoffset')
+            args.append(str(self.time_offset))
+        args.append('-i')
+        args.append(self.filename)
+        return args
+
+
+class _Ffmpeg_output_map:
+    def __init__(self, index: int | str, stream: int | None = None):
+        self.index = index
+        self.stream = stream
+
+    def args(self):
+        spec = str(self.index)
+        if self.stream is not None:
+            spec = f'{self.index}:{self.stream}'
+        return ['-map', spec]
+
+
+class _Ffmpeg_filter_complex:
+    def __init__(self, filter_str: str):
+        self.filter = filter_str
+
+    def args(self):
+        return ['-filter_complex', self.filter]
---
+++ subtitles.py
@@ -0,0 +1,160 @@
+
+from io import StringIO
+
+
+def fix_text(text):
+    return (text.strip()
+            .replace('| ', 'I ')
+            .replace('/ ', 'I ')
+            )
+
+
+class SubtitleEntry:
+    def __init__(self, text, start, end, style='Default', name='',
+                 marked=0, marginl=0, marginr=0, marginv=0, effect=''):
+        self.text = fix_text(text)
+        self.start = start
+        self.end = end
+        self.marked = int(marked)
+        self.style = style
+        self.name = name
+        self.marginl = int(marginl)
+        self.marginr = int(marginr)
+        self.marginv = int(marginv)
+        self.effect = effect
+
+    def _ts(self, sec: float, format='ssa'):
+        ms = (sec % 1)
+        sec = int(sec)
+        if format == 'ssa':
+            return (f'{int(sec / 3600):1d}:{int(sec / 60 % 60):02d}:'
+                    f'{sec % 60:02d}.{int(ms * 100):02d}')
+        return (f'{int(sec / 3600):02d}:{int(sec / 60 % 60):02d}:'
+                f'{sec % 60:02d},{int(ms * 1000):03d}')
+
+    def srt(self):
+        return (f'{self._ts(self.start, ",")} --> '
+                f'{self._ts(self.content["end"], ",")}\n'
+                f'{self.content["text"]}\n')
+
+    def ssa(self):
+        return (f'Dialogue: Marked={self.marked},{self._ts(self.start)},'
+                f'{self._ts(self.end)},{self.style},{self.name},'
+                f'{self.marginl:04d},{self.marginr:04d},{self.marginv:04d},'
+                f'{self.effect},{self.text.replace("\n", "\\n")}\n')
+
+    def __eq__(self, subtitle_entry):
+        return self.text == subtitle_entry.text
+
+
+class SubtitleSyle:
+    def __init__(self, name, fontname, fontsize=24,
+                 primarycolour=0xffffff, secondarycolour=0xffffff,
+                 tertiarycolour=0x000000, backcolour=0x000000,
+                 bold=-1, italic=0, borderstyle=1, outline=2,
+                 shadow=3, alignment=2, marginl=20, marginr=20,
+                 marginv=10, alphalevel=0, encoding=1):
+        self.name = name
+        self.fontname = fontname
+        self.fontsize = int(fontsize)
+        self.primarycolour = primarycolour
+        self.secondarycolour = secondarycolour
+        self.tertiarycolour = tertiarycolour
+        self.backcolour = backcolour
+        self.bold = int(bold)
+        self.italic = int(italic)
+        self.borderstyle = int(borderstyle)
+        self.outline = int(outline)
+        self.shadow = int(shadow)
+        self.alignment = int(alignment)
+        self.marginl = int(marginl)
+        self.marginr = int(marginr)
+        self.marginv = int(marginv)
+        self.alphalevel = int(alphalevel)
+        self.encoding = int(encoding)
+
+    def __str__(self):
+        return (f'Style: {self.name},{self.fontname},{self.fontsize},'
+                f'&H{self.primarycolour:06x},&H{self.secondarycolour:06x},'
+                f'&H{self.tertiarycolour:06x},&H{self.backcolour:06x},'
+                f'{self.bold},' f'{self.italic},'
+                f'{self.borderstyle},{self.outline},'
+                f'{self.shadow},{self.alignment},{self.marginl},'
+                f'{self.marginr},{self.marginv},{self.alphalevel},'
+                f'{self.encoding}')
+
+
+class Subtitles:
+    def __init__(self, width=720, height=480):
+        self.entries: list[SubtitleEntry] = []
+        self.styles: list[SubtitleSyle] = []
+        self.playresx = int(width)
+        self.playresy = int(height)
+
+#      def __init__(self, name, fontname, fontsize=24,
+#                   primarycolour=0xffffff, secondarycolour=0xffffff,
+#                   tertiarycolour=0x000000, backcolour=0x000000,
+#                   bold=-1, italic=0, borderstyle=1, outline=2,
+#                   shadow=3, alignment=2, marginl=20, marginr=20,
+#                   marginv=10, alphalevel=0, encoding=1):
+
+    def entry(self, text, start, end, style='Default', name='',
+              marked=0, marginl=0, marginr=0, marginv=10, effect=''):
+        self.entries.append(SubtitleEntry(text, start, end, style, name,
+                                          marked, marginl, marginr, marginv,
+                                          effect))
+
+    def style(self, name, fontname, fontsize=24,
+              primarycolour=0xffffff, secondarycolour=0xffffff,
+              tertiarycolour=0x000000, backcolour=0x000000,
+              bold=-1, italic=0, borderstyle=1, outline=2,
+              shadow=3, alignment=2, marginl=20, marginr=20,
+              marginv=10, alphalevel=0, encoding=1):
+        return self.styles.append(SubtitleSyle(
+            name, fontname, fontsize, primarycolour, secondarycolour,
+            tertiarycolour, backcolour, bold, italic, borderstyle, outline,
+            shadow, alignment, marginl, marginr, marginv, alphalevel,
+            encoding))
+
+    def srt(self):
+        buf = StringIO()
+        for i, entry in enumerate(self.entries):
+            buf.write(f'{i + 1}\n{entry.srt()}\n')
+        buf.seek(0)
+        return buf.read()
+
+    def ssa(self):
+        buf = StringIO()
+        buf.write('[Script Info]\n')
+        buf.write('; This is a Sub Station Alpha v4 script.\n')
+        buf.write('; For Sub Station Alpha info and downloads,\n')
+        buf.write('; go to http://www.eswat.demon.co.uk/\n')
+        buf.write('; or email kotus@eswat.demon.co.uk\n')
+        buf.write('; \n')
+        buf.write('; Note: This file was created with pysubtools.\n')
+        buf.write('; \n')
+        buf.write('ScriptType: v4.00\n')
+        buf.write('Collisions: Normal\n')
+        buf.write('PlayResX: 720\n')
+        buf.write('PlayResY: 480\n')
+        buf.write('Timer: 100.0000\n\n')
+
+        buf.write('[V4 Styles]\n')
+        buf.write('Format: Name, Fontname, Fontsize, PrimaryColour, '
+                  'SecondaryColour, TertiaryColour, BackColour, Bold, '
+                  'Italic, BorderStyle, Outline, Shadow, Alignment, '
+                  'MarginL, MarginR, MarginV, AlphaLevel, Encoding\n')
+        for style in self.styles:
+            buf.write(f'{style}\n')
+        buf.write('\n[Events]\n')
+        buf.write('Format: Marked, Start, End, Style, Name, MarginL, MarginR, '
+                  'MarginV, Effect, Text\n')
+
+        for entry in self.entries:
+            if entry.end < 0:
+                return ''
+            if entry.start < 0:
+                entry.start = 0
+            buf.write(entry.ssa())
+        buf.seek(0)
+        return buf.read()
