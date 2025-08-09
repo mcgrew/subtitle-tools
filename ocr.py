591354772b125bf56ea0e8b3d9a9421d3c1f417b
Thomas McGrew <tjmcgrew@gmail.com>
2025-08-09 17:50:17 -0400

    Added multiprocessing to speed up OCR

--- ocr.py
+++ ocr.py
@@ -6,12 +6,14 @@ from collections import defaultdict
 from typing import Iterator
 from dataclasses import dataclass
 from glob import glob
+from multiprocessing.pool import Pool
 from PIL import Image
 
 import ffmpeg
 from subtitles import Subtitles, SubtitleEntry
 
 FRAME_RATE = 10
+WORKDIR = '/tmp/work'
 
 
 @dataclass
@@ -84,7 +86,7 @@ def dump_subs(infile, stream):
     height = stream['height']
     st_index = stream['index']
 
-    ff = ffmpeg.Ffmpeg('work/%06d.png').skip('audio')
+    ff = ffmpeg.Ffmpeg(f'{WORKDIR}/%06d.png').skip('audio')
     ff.filter_complex(f"[1:v][0:{st_index}]overlay,mpdecimate[out]")
     ff.input(infile, None)
     ff.input(f"color=size={width}x{height}:rate={FRAME_RATE}:color=black",
@@ -110,6 +112,7 @@ def read_image(image) -> Iterator[TextLine]:
     builder.tesseract_flags.extend(['--oem', '1'])
     lineboxes = tool.image_to_string(pil_img, lang='eng', builder=builder)
     start_time = int(image[-10:-4]) / FRAME_RATE
+    results = []
     for linebox in lineboxes:
         ((x1, y1), (x2, y2)) = linebox.position
         r, g, b = text_color(pil_img, x1, y1, x2, y2)
@@ -126,11 +129,17 @@ def read_image(image) -> Iterator[TextLine]:
             size = int(size * 1.6)
         cropped = pil_img.crop((x1, y1, x2, y2))
         (_, r), (_, g), (_, b) = cropped.getextrema()
-        yield TextLine(start_time, linebox.content, size,
-                       marginl, marginr, marginv, (r, g, b))
+#          yield TextLine(start_time, linebox.content, size,
+#                         marginl, marginr, marginv, (r, g, b))
+        results.append(TextLine(start_time, linebox.content, size,
+                                marginl, marginr, marginv, (r, g, b)))
+    sys.stderr.write('=')
+    sys.stderr.flush()
+    return results
 
 
 def read_subs(directory) -> Iterator[TextLine]:
+    skip_cleanup = getenv('SUBCONVERT_SKIP_CLEANUP')
     sys.stderr.write('Performing OCR...\n')
 
     images = sorted(glob(f'{directory}/*.png'))
@@ -139,15 +148,16 @@ def read_subs(directory) -> Iterator[TextLine]:
     # add an extra "dummy" timestamp
     image_times.append(image_times[-1] + 5)
 
-    skip_cleanup = getenv('SUBCONVERT_SKIP_CLEANUP')
-    for i, image in enumerate(images):
-        sys.stderr.write(f'{int((i + 1) / len(images) * 100):3d}%\r')
-        sys.stderr.flush()
-        for line in read_image(image):
+    pool = Pool()
+    results = [pool.apply_async(read_image, (image,)) for image in images]
+
+    for i, result in enumerate(results):
+        for line in result.get():
             line.end = image_times[i+1]
             yield line
         if not skip_cleanup:
-            os.remove(image)
+            os.remove(images[i])
+    sys.stderr.write('\n')
 
 
 def sort_values(values: list[int]) -> list[int]:
@@ -197,16 +207,16 @@ def read_subtitles(infile, stream):
     skip_cleanup = getenv('SUBCONVERT_SKIP_CLEANUP')
     subs = Subtitles(stream['width'], stream['height'])
 
-    if os.path.exists('work'):
+    if os.path.exists(WORKDIR):
         sys.stderr.write('Work directory already present, '
                          'skipping export...\n')
     else:
-        os.mkdir('work')
+        os.mkdir(WORKDIR)
         sys.stderr.write('Exporting subpicture subtitles...\n')
 
     dump_subs(infile, stream)
     lines = []
-    for line in read_subs('work'):
+    for line in read_subs(WORKDIR):
         lines.append(line)
     normalize_values(lines, stream['height'])
     for line in lines:
@@ -222,7 +232,7 @@ def read_subtitles(infile, stream):
     if skip_cleanup:
         sys.stderr.write('Skipping cleanup..\n')
     else:
-        os.rmdir('work')
+        os.rmdir(WORKDIR)
     sys.stderr.write('OCR Complete. Please check the output for accuracy.\n')
 #      postprocess(subs)
     return subs
